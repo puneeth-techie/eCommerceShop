f7d384b7658cbea5961a09fd8f98323c201b64ad
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-02-17 17:27:22 +0530

    Navbar setup.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to eCommerce Shop</h1>
-    </div>
+    <>
+    <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to eCommerce Shop</h1>
+        </Container>
+      </main>
+    <Footer />
+    </>
   );
 }
 
