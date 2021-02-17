f4aa822c697309219c24d44ab3fb0fd9c8f37ba4
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-02-17 23:39:06 +0530

    Added Router functionality.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import HomeScreen from './screens/HomeScreen';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <>
-    <Header />
+    <Router>
+      <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
-    <Footer />
-    </>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/screens/ProductScreens.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductScreens = () => {
+  return <div>Product</div>;
+};
+
+export default ProductScreens;
