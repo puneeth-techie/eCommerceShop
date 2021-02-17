5a71802841225e79f6370d62763913b4dca471b1
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-02-17 15:33:56 +0530

    React setup for frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to eCommerce Shop</h1>
+    </div>
+  );
+}
+
+export default App;
