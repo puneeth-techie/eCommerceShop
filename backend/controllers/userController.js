72cfe41e3205cc6156b49c0109728cd2ead42d21
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-03-04 17:57:42 +0530

    Implementd user registration.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,44 @@ const authUser = asyncHandler(async (req, res, next) => {
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
-const getUserProfile = asyncHandler(async (req, res, next) => {});
+const getUserProfile = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
 
-export { authUser, getUserProfile };
+// @desc    Register user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  let user = await User.findOne({ email });
+  if (user) {
+    res.status(400);
+    throw new Error("Email already registered.");
+  } else {
+    user = await User.create({
+      name,
+      email,
+      password,
+    });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,6 +33,13 @@ userSchema.methods.matchPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
